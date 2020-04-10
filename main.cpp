37253f39ff2aa07f366268555c3ab35ed7264274
Aaron John Schlosser <aaron@aaronschlosser.com>
2020-04-09 21:06:04 -0700

    added Image and created renderCopyImage

--- main.cpp
+++ main.cpp
@@ -4,15 +4,10 @@
 #include <vector>
 #include <iostream>
 
-SDL_Window* window;
-SDL_Surface* surface;
-SDL_Surface* ui;
-SDL_Renderer* renderer;
-SDL_Texture* texture;
-SDL_Event event;
-SDL_DisplayMode dm;
-
-const int kTileSize = 64;
+struct Image {
+  SDL_Surface* surface;
+  SDL_Texture* texture;
+};
 
 struct Tile {
   int tileMapX;
@@ -20,86 +15,92 @@ struct Tile {
   std::string tileName;
 };
 
-
-std::vector<Tile> tiles;
-
-int main() {
-  SDL_Log("Initializing SDL libraries...");
-  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not initialize SDL: %s", SDL_GetError());
-    return 3;
-  } else SDL_Log("SDL initialized.");
-  if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not initialize SDL_image: %s", IMG_GetError());
-    return 3;
-  } else SDL_Log("SDL_image initialized.");
-  SDL_GetCurrentDisplayMode(0, &dm);
-  SDL_Log("Current display is %d by %d pixels.", dm.w, dm.h);
-
-  SDL_Surface *tilemap = IMG_Load("tilemap.png");
-  if (!tilemap) {
-    SDL_Log("IMG_Load error: %s", IMG_GetError());
-    return -1;
-  }
-  SDL_Log("Loaded tilemap is %dx%d pixel map of %dx%d pixel tiles.", tilemap->w, tilemap->h, kTileSize, kTileSize);
-  for (auto i = 0; i < tilemap->w; i += kTileSize) {
-    for (auto j = 0; j < tilemap->h; j += kTileSize) {
-      std::string name = "Tile " + std::to_string(i) + "x" + std::to_string(j);
-      Tile t{i,j,name};
-      tiles.push_back(t);
-      SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Created tile: %s", name.c_str());
-    }
-  }
-
-  if (SDL_CreateWindowAndRenderer(dm.w/2, dm.h/2, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-    return 3;
-  }
-
-  texture = SDL_CreateTextureFromSurface(renderer, tilemap);
-  if (!texture) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-    return 3;
-  }
-
-  ui = SDL_CreateRGBSurface(0, dm.w, dm.h, 16, 0, 0, 0, 255);
-  SDL_FillRect(ui, NULL, SDL_MapRGB(ui->format, 0, 0, 255));
-  SDL_Texture *uit = SDL_CreateTextureFromSurface(renderer, ui);
-
-  SDL_FreeSurface(ui);
-  SDL_FreeSurface(surface);
-  SDL_FreeSurface(tilemap);
-
-  bool running = true;
-  while (running) {
-    SDL_PollEvent(&event);
-    if (event.type == SDL_QUIT) {
-      running = false;
-    }
-    else if (event.type == SDL_KEYDOWN) {
-      switch(event.key.keysym.sym) {
-        case SDLK_ESCAPE:
-          running = false;
-          break;
+struct GameEngine {
+  bool running;
+  SDL_Window* appWindow;
+  SDL_Surface* gameSurface;
+  SDL_Surface* uiSurface;
+  SDL_Renderer* appRenderer;
+  Image tilemapImage;
+  SDL_Event appEvent;
+  SDL_DisplayMode displayMode;
+  const int tileSize;
+  std::vector<Tile> tiles;
+  GameEngine() : tileSize(64), running(true) {}
+  int init()
+  {
+    SDL_Log("Initializing SDL libraries...");
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)) {
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not initialize SDL: %s", SDL_GetError());
+      return 3;
+    } else SDL_Log("SDL initialized.");
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not initialize SDL_image: %s", IMG_GetError());
+      return 3;
+    } else SDL_Log("SDL_image initialized.");
+    SDL_Surface *surface = IMG_Load("tilemap.png");
+    if (!surface) {
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "IMG_Load error: %s", IMG_GetError());
+      return 3;
+    } else SDL_Log("Loaded spritesheet is %dx%dpx sheet of %dx%dpx tiles.", surface->w, surface->h, tileSize, tileSize);
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+    SDL_Log("Current display is %dx%dpx.", displayMode.w, displayMode.h);
+    if (SDL_CreateWindowAndRenderer(displayMode.w/2, displayMode.h/2, SDL_WINDOW_RESIZABLE, &appWindow, &appRenderer)) {
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+      return 3;
+    } else SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Window created.");
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(appRenderer, surface);
+    if (!texture) {
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
+      return 3;
+    } else SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Tilemap loaded.");
+    tilemapImage = {surface, texture};
+    for (auto i = 0; i < surface->w; i += tileSize) {
+      for (auto j = 0; j < surface->h; j += tileSize) {
+        std::string name = "Tile " + std::to_string(i) + "x" + std::to_string(j);
+        Tile t{i,j,name};
+        tiles.push_back(t);
+        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Created tile: %s", name.c_str());
       }
     }
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_Rect dest{0,0,100,100};
-    SDL_RenderCopy(renderer, uit, NULL, &dest);
-    //Tile t = tiles.at(12);
-    //SDL_Rect r_src{t.tileMapX, t.tileMapY, kTileSize, kTileSize};
-    //SDL_Rect r_dst{0, 0, 64, 64};
-    //SDL_RenderCopy(renderer, texture, &r_src, &r_dst);
-    SDL_RenderPresent(renderer);
+    return 0;
   }
+  int quit()
+  {
+    return 0;
+  }
+  void renderCopyImage(SDL_Renderer* renderer, Image* i, int x, int y)
+  {
+    SDL_Rect dest {x, y, i->surface->w, i->surface->h};
+    SDL_RenderCopy(renderer, i->texture, NULL, &dest);
+  }
+  int run()
+  {
+    running = true;
+    while (running) {
+      SDL_PollEvent(&appEvent);
+      if (appEvent.type == SDL_QUIT) {
+        running = false;
+      }
+      else if (appEvent.type == SDL_KEYDOWN) {
+        switch(appEvent.key.keysym.sym) {
+          case SDLK_ESCAPE:
+            running = false;
+            break;
+        }
+      }
+      SDL_SetRenderDrawColor(appRenderer, 0x00, 0x00, 0x00, 0x00);
+      SDL_RenderClear(appRenderer);
+      renderCopyImage(appRenderer, &tilemapImage, 300, 300);
+      SDL_RenderPresent(appRenderer);
+    }
+    return 1;
+  }
+};
 
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-
-  IMG_Quit();
-  SDL_Quit();
+int main() {
+  GameEngine engine;
+  engine.init();
+  engine.run();
   return 0;
 }
